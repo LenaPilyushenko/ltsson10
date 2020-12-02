45d181461a0fe0e84962f2e597601cc0f314728e
Lena Pilyushenko <elena_pilyushenko@mail.ru>
2020-12-02 12:06:23 +0300

    feat(js/script.js):new feat

---
+++ script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const parentBlock =  document.querySelector('.books'),
+    itemsBook = document.querySelectorAll('.book'),
+    itemBody = document.getElementsByTagName('body')[0],
+    titeItemBook = itemsBook[4].getElementsByTagName('h2')[0],
+    parentItemBook2 = itemsBook[0].querySelector('ul'),
+    collectionItemBook2 = itemsBook[0].querySelectorAll('li'),
+    parentItemBook5 = itemsBook[5].querySelector('ul'),
+    collectionItemBook5 = itemsBook[5].querySelectorAll('li')
+;
+
+parentBlock.prepend(itemsBook[1]);
+parentBlock.append(itemsBook[2]);
+itemsBook[3].before(itemsBook[4]);
+
+itemBody.setAttribute('style', 'background-image: url(./image/you-dont-know-js.jpg);');
+
+titeItemBook.innerHTML = '<a href = "">Книга 3. this и Прототипы Объектов"</a>';
+
+document.querySelector('.adv').remove();
+
+parentItemBook2.append(collectionItemBook2[7]);
+parentItemBook2.append(collectionItemBook2[9]);
+parentItemBook2.append(collectionItemBook2[2]);
+parentItemBook2.append(collectionItemBook2[10]);
+collectionItemBook2[4].before(collectionItemBook2[6]);
+collectionItemBook2[4].before(collectionItemBook2[8]);
+
+collectionItemBook5[2].before(collectionItemBook5[9]);
+collectionItemBook5[6].before(collectionItemBook5[2]);
+collectionItemBook5[8].before(collectionItemBook5[5]);
