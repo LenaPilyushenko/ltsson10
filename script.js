bae9d63be397a5905d831118e430ecfc8273d45e
Lena Pilyushenko <elena_pilyushenko@mail.ru>
2020-12-02 15:23:11 +0300

    fix(js/script.js): new feat

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ const parentBlock =  document.querySelector('.books'),
     parentItemBook2 = itemsBook[0].querySelector('ul'),
     collectionItemBook2 = itemsBook[0].querySelectorAll('li'),
     parentItemBook5 = itemsBook[5].querySelector('ul'),
-    collectionItemBook5 = itemsBook[5].querySelectorAll('li')
+    collectionItemBook5 = itemsBook[5].querySelectorAll('li'),
+    parentItemBook6 = itemsBook[2].querySelector('ul'),
+    collectionItemBook6 = itemsBook[2].querySelectorAll('li'),
+    newElement = document.createElement('li')
 ;
 
 parentBlock.prepend(itemsBook[1]);
@@ -30,3 +33,13 @@ collectionItemBook2[4].before(collectionItemBook2[8]);
 collectionItemBook5[2].before(collectionItemBook5[9]);
 collectionItemBook5[6].before(collectionItemBook5[2]);
 collectionItemBook5[8].before(collectionItemBook5[5]);
+
+newElement.textContent = 'Глава 8: За пределами ES6';
+
+collectionItemBook6[9].before(newElement);
+
+console.log ('itemsBook', itemsBook);
+console.log ('parentItemBook6', parentItemBook6);
+console.log ('collectionItemBook6', collectionItemBook6);
+ 
+console.log (newElement);
